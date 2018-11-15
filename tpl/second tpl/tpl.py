90f795f7ea5ccb016a60bb67668ae56fd17da830
HCelante <higor.celante@gmail.com>
2018-11-15 17:55:43 -0200

    Retorno TPL
    a função tpl retorna os sinonimos em uma lista (tpl 0.3)

--- tpl/second tpl/tpl.py
+++ tpl/second tpl/tpl.py
@@ -2,7 +2,8 @@ from lxml import html
 import requests
 from bs4 import BeautifulSoup
 import html2text  
-
+#######################################################################################
+##################   RETORNA A LISTA DE SINÔNIMOS #####################################
 def tpl(name):
     search_url = "http://www.theplantlist.org/tpl1.1/search?q="
     ########################
@@ -10,33 +11,31 @@ def tpl(name):
     #CHAVES DE PESQUISA
     key1 = name.replace(" ", "+")
     ######################
-    #  print (key1)
+
     #STRING DE BUSCA QUE SERA CONCATENADA NO LINK
     search = search_url + key1
     #######################################
-    #  print (search)
+
     #REQUISICAO DE CONTEUDO
     searc = requests.get(search)
     webpage = html.fromstring(searc.content)
     href = webpage.xpath('//a/@href')
     conca = str(href[23]) + "?ref=tpl1"
-    print (conca)
     tena = "http://www.theplantlist.org"
     concatena = tena + conca  
-    #print (concatena)
+    print(concatena)
     content = requests.get(concatena).content
     ######################################
 
     #BS4 ORGANIZACAO
     soup = BeautifulSoup(content, "lxml")
-    #print (soup)
-    #all_tables = soup.find_all('table')
+
     
     text = soup.get_text()
     text = text.replace("  "," ")
 
-    #text = text[871:]
-    #print (text)
+
+
     sino = []
     acum = {}
     i = 0
@@ -53,27 +52,15 @@ def tpl(name):
     while (acuma != "tplTableInit"):
         acuma = sino[j:j+12]
         j+=1
-    final = sino[:j-1]
-    print (final)
+    final =sino[:j-1].strip("\n").split('  ')
+
+    return (final)
+
+########################################################################################    
 
         
 
     
-tpl("Panicum prionitis Nees")
-
-
-"""CREATE TABLE PLANTATHEPLANTLIST(
-	ID INTEGER NOT NULL auto_increment,
-	PLANTA VARCHAR(200),	
-	AUTOR VARCHAR(200),
-	PRIMARY KEY(ID)
-);
-
-CREATE TABLE SINONIMOS_PLANTLIST(
-	ID INTEGER NOT NULL auto_increment,PLANTATHEPLANTLIST	
-	NOME VARCHAR (100),
-	ID_PLANTA_CORRETA INTEGER,	
-	AUTOR VARCHAR(200),	
-	FOREIGN KEY(ID_PLANTA_CORRETA) REFERENCES PLANTATHEPLANTLIST(ID),
-	PRIMARY KEY(ID)
-);"""
+final = tpl("Panicum prionitis Nees")
+for i in range(len(final)):
+    print (final[i])
