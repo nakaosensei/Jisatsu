8030766ce7165c7f76084f0589996bab5526e395
nakaosensei <nakaosensei@gmail.com>
2018-11-27 21:10:36 -0200

    remocaoFoldersAntigas

--- scripts/gbif/src/FR/run.py
+++
@@ -1,6 +0,0 @@
-
-import XLS
-import sys
-
-if __name__ == "__main__":
-    xlsx = XLS.xlxs(sys.argv[0])
--- scripts/specieslink/fileNk.py
+++
@@ -1,6 +0,0 @@
-class File:
-
-    def writeToFile(self,fileName,content):
-        file = open(fileName,"w")
-        file.write(content)
-        file.close()
